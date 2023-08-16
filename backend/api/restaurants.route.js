4741f6088768a3973b8bfd360d5d9bf202511b52
jamesjiayu <jamesjiayu@users.noreply.github.com>
2023-08-16 23:21:50 +0000

    failed post

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,8 +1,30 @@
+// import express from "express"
+// import ReviewsCtrl from "./reviews.controller.js"
+// import RestauRantsCtrl from "./restaurants.controller.js"
+// const router= express.Router()
+// router.route("/").get(RestauRantsCtrl.apiGetRestaurants)
+// router.route("/review")
+//     .post(ReviewsCtrl.apiPostReview)
+//     .put(ReviewsCtrl.apiUpdateReview )
+//     .delete(ReviewsCtrl.apiDeleteReview)
+// export default router
+
+
+
 import express from "express"
-const router= express.Router()
-router.route("/").get((req,res)=>res.send('hello world'))
+import RestaurantsCtrl from "./restaurants.controller.js"
+import ReviewsCtrl from "./reviews.controller.js"
+
+const router = express.Router()
+
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
+
+router
+  .route("/review")
+  .post(ReviewsCtrl.apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
-
-
-
