db535c6036e68e60d356958edbd785e8031c580b
jamesjiayu <jamesjiayu@users.noreply.github.com>
2023-08-16 23:23:18 +0000

    post data in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,9 @@ app.use("/api/v1/restaurants",restaurants)
 app.use("*", (req,res)=>res.status(400).json({error: "not found"}) )
 
 export default app
+// {
+//     "restanrant_id": "5eb3d668b31de5d588f42933",
+//     "text":"haha",
+//     "user_id": "7777777", 
+//     "name":"j"
+//     }
